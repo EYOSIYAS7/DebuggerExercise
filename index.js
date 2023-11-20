c4f48d04a8e168acf1ee29fad5bf90f1bbdf0d80
EYOSIYAS <eyosiyasmengesha@gmail.com>
2023-11-20 22:51:44 +0300

    use the vscode debugger

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const input = document.querySelectorAll(".input");
-
+const nameinput = document.querySelector(".nameinput");
 const btn = document.querySelector(".btn");
 const result = document.querySelector(".result");
 
@@ -10,11 +10,11 @@ const data = [
   { id: 4, title: "arm" },
 ];
 const getUsername = () => {
-  return input[0].value;
+  return nameinput.value;
 };
 
 const getage = () => {
-  return input[1].value;
+  return input.value;
 };
 const incrementAge = (number) => {
   const parserdnumber = parseInt(number);
@@ -24,18 +24,24 @@ const incrementAge = (number) => {
 btn.addEventListener("click", (e) => {
   console.log("clicked");
   e.preventDefault();
-  debugger;
+  // debugger;
   const userN = getUsername();
   const age = getage();
   const incrementedAge = incrementAge(age);
   console.log(userN);
   result.textContent = `${userN} ${age} the incremented age ${incrementedAge}`;
 });
-input.addEventListener("change", (e) => {
+nameinput.addEventListener("change", (e) => {
   e.preventDefault();
+  console.log("change event is fired");
   const userN = getUsername();
-  const newInput = userN.toLowercase();
+  console.log(userN);
+
   const filterd = data.filter((item) =>
-    item.title.toLowerCase().includes(newInput)
+    item.title.toLowerCase().includes(userN.toLowerCase())
   );
+  // the reason is this that before u didn't see the value of the variable filterd b/c the break point
+  // was set before the filterd was instantiated thats way it says uncaught referenceError
+
+  console.log(filterd);
 });
